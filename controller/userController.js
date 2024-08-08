45af92b6ee389ad4e83ae0f5e0447eb7be551f92
RashedujjamanNoor <noorrk042@gmail.com>
2024-08-09 01:27:39 +0600

    finished backend work

--- controller/userController.js
+++ controller/userController.js
@@ -16,3 +16,62 @@ export const create = async (req, res) => {
     });
   }
 };
+
+export const showUsers = async (req, res) => {
+  try {
+    const showData = await User.find();
+    if (!showData) {
+      return res.status(404).json({ msg: "no user found" });
+    }
+
+    res.status(200).json(showData);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+};
+
+export const singleUser = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const showUser = await User.findById(userId);
+    if (!showUser) {
+      return res.status(404).json({
+        msg: "user not found",
+      });
+    }
+    res.status(200).json(showUser);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const existUser = await User.findById(userId);
+    if (!existUser) {
+      res.status(404).json({ msg: "user not exist" });
+    }
+    await User.findByIdAndDelete(existUser);
+    res.status(200).json({ msg: "user deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const existUser = await User.findById(userId);
+    if (!existUser) {
+      return res.status(404).json({ msg: "user not exist" });
+    }
+
+    const updatedData = await User.findByIdAndUpdate(userId, req.body, {
+      new: true,
+    });
+    res.status(200).json(updatedData);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+};
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,18 @@
 import express from "express";
-import { create } from "../controller/userController.js";
+import {
+  create,
+  deleteUser,
+  showUsers,
+  singleUser,
+  updateUser,
+} from "../controller/userController.js";
 
 const route = express.Router();
 
 route.post("/create", create);
+route.get("/users", showUsers);
+route.get("/user/:id", singleUser);
+route.delete("/user/:id", deleteUser);
+route.put("/update/:id", updateUser);
 
 export default route;
